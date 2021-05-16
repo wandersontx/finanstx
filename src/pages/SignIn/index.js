ea1bebc345866781231650f1dc1faa325d118c48
wandersontx <wandersonh89@gmail.com>
2021-05-16 11:48:45 -0300

    Tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+
+import { 
+  Background, Container, Logo, AreaInput, Input,
+  SubmitButton, SubmitText, Link, LinkText,
+} from './styles';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+
  return (
-   <View>
-       <Text>SignIn</Text>
-   </View>
+   <Background>
+      <Container>
+        <Logo source={require('../../assets/img/Logo.png')} />
+
+        <AreaInput>
+          <Input 
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={email}
+            onChangeText={ (texto) => setEmail(texto) }
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input 
+            placeholder="Senha"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={senha}
+            onChangeText={ (texto) => setSenha(texto) }
+          />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>
+            Acessar
+          </SubmitText>
+        </SubmitButton>
+
+        <Link>
+          <LinkText>
+            Criar uma conta
+          </LinkText>
+        </Link>
+      </Container>
+   </Background>
   );
 }
